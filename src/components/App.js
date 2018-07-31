e3c10fe2ba4e6d1ee51f3e956d02ca35d092e22c
Alyssa <allygator95@gmail.com>
2018-07-31 01:48:28 -0400

    Reputations now show up, added some styling

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
 import RealmsList from './Realms';
-//import Reputation from './Reputations';
+import Reputation from './Reputations';
 
 //import {blizzardKey} from '../API_Keys';
 
@@ -10,38 +10,52 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.setRealmState = this.setRealmState.bind(this);
-        this.makeReputationRequest = this.makeReputationRequest.bind(this);
+        this.showReputations = this.showReputations.bind(this);
+        this.state = {
+            reputationsHidden: false
+        }
     }
+
+    setRealmState(a) {
+        console.log(a);
+        this.setState({realm: a})
+    }
+
+
+    showReputations(e) {
+        this.setState({reputationsHidden: true});
+        //return <Reputation realm="realmName" name="characterName" />;
+    }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <RealmsList realmSelection={this.setRealmState} /*specificRealm="Quel'Dorei"*//>
-        <div id="name">
-            Character Name:
-            <input type="text" id="characterName" name="Character Name" onChange={e=>this.setState({name:e.target.value})}/>
-        </div>
-        <input type="button" value="Submit" onClick={() => this.makeReputationRequest(this.state.realm, this.state.name)}/*<Reputations name:this.name, realm:this.realm />*/ id="submitButton" />
-      </div>
-    );
+      const reputationsHidden = this.state.reputationsHidden;
+      let repPanel;
+      if(reputationsHidden) {
+          repPanel = <Reputation name={this.state.name} realm={this.state.realm} />;
+      }
+      return (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to RepCalc</h1>
+            </header>
+            <p className="App-intro">
+              Select your realm from the dropdown and type your character name, then submit.
+            </p>
+            <div className="user-input-box">
+            <RealmsList realmSelection={this.setRealmState} /*specificRealm="Quel'Dorei"*//>
+            <div id="name">
+                Character Name:
+                <input type="text" id="characterName" name="Character Name" onChange={e=>this.setState({name:e.target.value})}/>
+            </div>
+            <input type="button" value="Submit" onClick={this.showReputations}/*<Reputations name:this.name, realm:this.realm />*/ id="submitButton" />
+          </div>
+          {repPanel}
+          </div>
+      );
   }
 
-  setRealmState(a) {
-      console.log(a);
-      this.setState({realm: a})
-  }
 
-
-  makeReputationRequest(realmName,characterName) {
-      console.log(realmName + " " + characterName);
-      //<Reputation realm="realmName" name="characterName" />
-  }
 }
 
 export default App;
--- src/components/Reputations.js
+++ src/components/Reputations.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import {blizzardKey} from '../API_Keys';
+var bestFriends = [1273, 1275, 1276, 1277, 1278, 1279, 1280, 1281, 1282, 1283, 1975, 1358]; //IDs for NPCs that have "Friend" levels rather than reputations
+var friendLevels = ["Stranger","Acquantaince", "Buddy", "Friend", "Good Friend", "Best Friend"];
+var repTitles = ["Hated", "Hostile", "Unfriendly", "Neutral", "Friendly", "Honored", "Revered", "Exalted"]; // Reputation levels
 
 class Reputation extends Component {
     constructor(props) {
@@ -7,15 +10,18 @@ class Reputation extends Component {
         this.state = {
             error: null,
             isLoaded: false,
-            reps: []
+            reps: [],
+            max: false,
             //selectedRealm: "",
             //selectedOption: null
         };
         //this.realmSelection = this.realmSelection.bind(this);
+        this.isMaxRep = this.isMaxRep.bind(this);
+        this.resetRep = this.resetRep.bind(this);
     }
 
     componentDidMount() {
-        fetch('https://us.api.battle.net/wow/character/' + this.realm + '/' + this.name + '?fields=reputation&locale=en_US' + blizzardKey)
+        fetch('https://us.api.battle.net/wow/character/' + this.props.realm + '/' + this.props.name + '?fields=reputation&locale=en_US' + blizzardKey)
             .then(response => response.json())
             .then((repList) => {
                 this.setState({
@@ -32,9 +38,20 @@ class Reputation extends Component {
             )
     }
 
+    isMaxRep(rep) {
+        if (bestFriends.includes(rep.id) && rep.standing === 5)
+            this.setState({max: true});
+        if (rep.standing === 7)
+            this.setState({max: true});
+    }
+
+    resetRep() {
+        this.setState({max: false});
+    }
+
     render() {
         const { error, isLoaded, reps } = this.state;
-        console.log();
+        //console.log(this.props.name);
         if (error) {
           return <div>Error: {error.message}</div>;
         } else if (!isLoaded) {
@@ -42,9 +59,25 @@ class Reputation extends Component {
         } else {
             return (
                 reps.map((rep) => (
-                  <h3>{rep.name}</h3>
-                ))
+                        //console.log(rep);
+                        //{this.isMaxRep(rep)}
+                        <div key={rep.name}>
+                        <h3>{rep.name}</h3>
+                        <p>{repTitles[rep.standing]}</p>
+                        <p>{rep.value}/{rep.max}</p>
+                        </div>
+                        //{this.resetRep()}
+                )
+            )
             );
+            // realms.map(
+            //     function(realm) {
+            //       return {
+            //         value: realm.name,
+            //         label: realm.name
+            //       };
+            //     }
+            //   )
         }
     }
 }
