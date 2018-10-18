18d531ee4291335c055f6867179c5e2975a3d7dd
Alyssa Davis <alyssa.davis@ucf.edu>
2018-10-18 14:23:59 -0400

    I have absolutely no idea where this function lives, but I removed stuff

--- functions/token.js
+++ functions/token.js
@@ -1,5 +1,5 @@
-const client = process.env.clientID;
-const secret = process.env.secret;
+/*const client = process.env.clientID;
+const secret = process.env.secret;*/
 
 exports.handler = function(event, context, callback) {
     console.log("testing function");
