4abef239dc23bfab37f495564dbd93ffea620fcc
Alyssa Davis <alyssa.davis@ucf.edu>
2018-10-18 15:40:35 -0400

    testing out the variables

--- functions/token.js
+++ functions/token.js
@@ -1,6 +1,9 @@
+const clientID = process.env.clientID;
+
 exports.handler = async (event, context, callback) => {
+    console.log("test");
   return callback(null, {
     statusCode: 200,
-    body: "test"
+    body: clientID;
 });
 };
