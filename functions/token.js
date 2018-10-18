3de2f196e7c1bca824811731f12d7540771b3638
Alyssa Davis <alyssa.davis@ucf.edu>
2018-10-18 13:41:24 -0400

    I have no idea what Im doing

--- functions/token.js
+++ functions/token.js
@@ -2,6 +2,7 @@ const client = process.env.clientID;
 const secret = process.env.secret;
 
 exports.handler = function(event, context, callback) {
+    console.log("testing function");
     return {
     statusCode: 200,
     body: client
