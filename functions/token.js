f7b3def1d05a66683e6c687e72c3e25ea176c0d7
Alyssa Davis <alyssa.davis@ucf.edu>
2018-10-19 13:08:09 -0400

    going back to import

--- functions/token.js
+++ functions/token.js
@@ -1,5 +1,4 @@
-'use strict';
-const fetch = require('node-fetch');
+import fetch from "node-fetch";
 
 const endpoint = 'https://us.battle.net/oauth/token?grant_type=client_credentials&client_id='+process.env.CLIENTID+'&client_secret='+process.env.SECRET;
 
