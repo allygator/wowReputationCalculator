7ece8d17d593b0e1163d01485de0972c4220698a
Alyssa Davis <alyssa.davis@ucf.edu>
2018-10-19 11:43:25 -0400

    I give up

--- functions/token.js
+++ functions/token.js
@@ -1,5 +1,5 @@
 'use strict';
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
 const endpoint = 'https://us.battle.net/oauth/token?grant_type=client_credentials&client_id='+process.env.CLIENTID+'&client_secret='+process.env.SECRET;
 
