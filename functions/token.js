71f27a3407208d5e003bf66056165d555800d4a9
Alyssa <allygator95@gmail.com>
2018-10-18 21:11:36 -0400

    IT WORKS.. sort of

--- functions/token.js
+++ functions/token.js
@@ -1,8 +1,9 @@
-const clientID = process.env.CLIENTID
+const clientID = process.env.CLIENTID;
+const secret = process.env.SECRET;
 
 exports.handler = async (event, context, callback) => {
   return callback(null, {
     statusCode: 200,
-    body: clientID
+    body: secret
     })
 };
