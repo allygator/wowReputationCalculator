b05c94ee5169437563ce303602ea563044a1a1fc
Alyssa <allygator95@gmail.com>
2018-10-18 21:39:54 -0400

    does this version of importing work

--- functions/token.js
+++ functions/token.js
@@ -1,4 +1,4 @@
-//import fetch from "node-fetch";
+const fetch = require("node-fetch");
 
 const endpoint = 'https://us.battle.net/oauth/token?grant_type=client_credentials&client_id='+process.env.CLIENTID+'&client_secret='+process.env.SECRET;
 
