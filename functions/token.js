92e9fc01a3786cd59ae827f8d4ebf8acfd5f767f
Alyssa Davis <alyssa.davis@ucf.edu>
2018-10-19 11:40:30 -0400

    maybe?

--- functions/token.js
+++ functions/token.js
@@ -1,3 +1,4 @@
+'use strict';
 const fetch = require("node-fetch");
 
 const endpoint = 'https://us.battle.net/oauth/token?grant_type=client_credentials&client_id='+process.env.CLIENTID+'&client_secret='+process.env.SECRET;
