b15dc88e31d6ad4a3b326ec0a1c4ae20ce535dbe
Alyssa <allygator95@gmail.com>
2018-10-21 15:26:19 -0400

    calling a lambda

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import '../App.css';
 import Header from './Header';
 import Main from './Main';
-const fetch = require('node-fetch');
 
 class App extends Component {
     render() {
