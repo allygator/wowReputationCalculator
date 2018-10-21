04f824520446e728906c9b15288f76ed22e82635
Alyssa <allygator95@gmail.com>
2018-10-21 12:35:30 -0400

    MAYBE IT WORKS?!

---
+++ src/lambda/gettoken.js
@@ -0,0 +1,20 @@
+var axios = require( 'axios');
+const firebase_auth = process.env.FIREBASE_AUTH;
+const firebase_url = process.env.FIREBASE_URL;
+
+ exports.handler = function(event, context, callback) {
+    axios.post(firebase_auth)
+        .then((authResponse) => {
+            var auth = authResponse.data.idToken;
+            const firebase_endpoint = firebase_url+"?auth="+auth
+            return axios(firebase_endpoint)
+        })
+        .then((databaseResponse) => {
+            var token = databaseResponse.data.token;
+            callback(null, {
+                statusCode: 200,
+                body: token
+            });
+        })
+        .catch((error) => callback(error.message))
+ }
