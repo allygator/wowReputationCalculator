965711801adea9d7e43af11e8178cc2075b850a7
Alyssa <allygator95@gmail.com>
2018-10-21 11:53:52 -0400

    Remove references to function

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,6 @@ import Main from './Main';
 const fetch = require('node-fetch');
 
 class App extends Component {
-    // constructor(props) {
-    //     super(props);
-    //
-    // }
-
-    componentDidMount() { fetch('/.netlify/functions/token', {
-        method: 'POST'
-  }).then(response => {
-    console.log(response);
-  })
-}
-
     render() {
         return (
           <div className="App">
