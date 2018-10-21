b7c6d79841d0d119d84754bf463a1228fc0a2205
Alyssa <allygator95@gmail.com>
2018-10-20 21:38:50 -0400

    WHO KNOWS ANYMORE

---
+++ src/lambda/token.js
@@ -0,0 +1,16 @@
+var axios = require( 'axios');
+
+const endpoint = 'https://us.battle.net/oauth/token?grant_type=client_credentials&client_id='+process.env.CLIENTID+'&client_secret='+process.env.SECRET;
+
+exports.handler = async (event, context, callback) => {
+    axios(endpoint)
+    .then((realmList) => {
+        // console.log(realmList);
+    })
+    .catch(function(goaway) {})
+
+    callback(null, {
+        statusCode: 200,
+        body: endpoint
+    });
+}
