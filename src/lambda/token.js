b85e29722a592431f07eead657a93d1a69bd3474
Alyssa <allygator95@gmail.com>
2018-10-21 12:06:30 -0400

    CACHE?! ARE YOU KIDDING ME?!

--- src/lambda/token.js
+++ src/lambda/token.js
@@ -1,11 +1,11 @@
-/*var axios = require( 'axios');
+var axios = require( 'axios');
 
 const token_endpoint ='https://us.battle.net/oauth/token?grant_type=client_credentials&client_id='+process.env.CLIENTID+'&client_secret='+process.env.SECRET;
 const firebase_auth = process.env.FIREBASE_AUTH;
-const firebase_url = process.env.FIREBASE_URL;*/
+const firebase_url = process.env.FIREBASE_URL;
 
 exports.handler = function(event, context, callback) {
-    /*axios(token_endpoint)
+    axios(token_endpoint)
         .then((tokenResponse) => {
             var token = tokenResponse.data.access_token;
             axios.post(firebase_auth)
@@ -31,9 +31,5 @@ exports.handler = function(event, context, callback) {
                 body: "You're not supposed to be here."
             });
         })
-        .catch(function(error){callback(error.message)})*/
-        callback(null, {
-            statusCode: 200,
-            body: "You're not supposed to be here."
-        });
+        .catch(function(error){callback(error.message)})
 }
