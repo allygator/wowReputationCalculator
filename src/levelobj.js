2ecb92b5c0aa3fc81f51980bb09cbc2e0be8116a
Alyssa <allygator95@gmail.com>
2020-08-08 20:42:59 -0400

    Added the first ways to earn reps

---
+++ src/levelobj.js
@@ -0,0 +1,60 @@
+// frequency: 1 for daily, 2 for weekly, 3 for unlimited
+const level = {
+  "1105": {
+    quest: [
+      {
+        req: "neutral",
+        amount: 700,
+        group: true,
+        items: [{ name: "Appeasing the Great Rain Stone", id: 12704 }],
+        frequency: 1,
+      },
+      {
+        req: "neutral",
+        amount: 350,
+        group: true,
+        items: [
+          { name: "Mastery of the Crystals", id: 12761 },
+          { name: "Power of the Great Ones", id: 12762 },
+          { name: "Will of the Titans", id: 12705 },
+        ],
+        frequency: 1,
+      },
+      {
+        req: "neutral",
+        amount: 350,
+        group: true,
+        items: [
+          { name: "A Cleansing Song", id: 12735 },
+          { name: "Song of Fecundity", id: 12737 },
+          { name: "Song of Reflection", id: 12736 },
+          { name: "Song of Wind and Water", id: 12726 },
+        ],
+        frequency: 1,
+      },
+    ],
+  },
+  "1073": {
+    quest: [
+      { name: "Zone Quests", notes: "Will get you to revered" },
+      {
+        req: "neutral",
+        amount: 250,
+        items: [{ name: "Preparing for the Worst", id: 11945 }],
+        frequency: 1,
+      },
+      {
+        req: "neutral",
+        amount: 250,
+        items: [{ name: "The Way to His Heart...", id: 11472 }],
+        frequency: 1,
+      },
+      {
+        req: "neutral",
+        amount: 250,
+        items: [{ name: "Planning for the Future", id: 11960 }],
+        frequency: 1,
+      },
+    ],
+  },
+};
