3fdaeb9e1967ff6b7e65508ca3a364c00b9e755c
Alyssa Davis <allygator95@gmail.com>
2020-09-11 13:31:16 -0400

    Fix lambda function

--- src/lambda/updatetoken.js
+++ src/lambda/updatetoken.js
@@ -1,43 +1,43 @@
 var axios = require("axios");
 
 const token_endpoint =
-  "https://us.battle.net/oauth/token?grant_type=client_credentials&client_id=" +
-  process.env.CLIENTID +
-  "&client_secret=" +
-  process.env.SECRET;
+	"https://us.battle.net/oauth/token?grant_type=client_credentials&client_id=" +
+	process.env.CLIENTID +
+	"&client_secret=" +
+	process.env.SECRET;
 const firebase_auth = process.env.FIREBASE_AUTH;
 const firebase_url = process.env.FIREBASE_URL;
 let firebase_endpoint = firebase_url;
 
 exports.handler = function (event, context, callback) {
-  var token;
-  axios(token_endpoint)
-    .then((tokenResponse) => {
-      token = tokenResponse.data.access_token;
-      return axios.post(firebase_auth);
-    })
-    .then((authResponse) => {
-      var auth = authResponse.data.idToken;
-      firebase_endpoint = firebase_endpoint + "?auth=" + auth;
-      return axios(firebase_endpoint);
-    })
-    .then((databaseResponse) => {
-      var oldtoken = databaseResponse.data.token;
-      if (oldtoken !== token) {
-        console.log("Token updated");
-        axios
-          .put(firebase_endpoint, { token: token })
-          .then((res) => {})
-          .catch(function (error) {
-            callback(error.message);
-          });
-      }
-    })
-    .then((response) => {
-      callback(null, {
-        statusCode: 200,
-        body: "You're not supposed to be here.",
-      });
-    })
-    .catch((error) => callback(error.message));
+	var token;
+	axios(token_endpoint)
+		.then((tokenResponse) => {
+			token = tokenResponse.data.access_token;
+			return axios.post(firebase_auth);
+		})
+		.then((authResponse) => {
+			var auth = authResponse.data.idToken;
+			firebase_endpoint = firebase_endpoint + "?auth=" + auth;
+			return axios(firebase_endpoint);
+		})
+		.then((databaseResponse) => {
+			var oldtoken = databaseResponse.data.token;
+			if (oldtoken !== token) {
+				console.log("Token updated");
+				axios
+					.put(firebase_endpoint, { token: token })
+					.then((res) => {})
+					.catch(function (error) {
+						callback(error.message);
+					});
+			}
+		})
+		.then((response) => {
+			callback(null, {
+				statusCode: 200,
+				body: "You're not supposed to be here.",
+			});
+		})
+		.catch((error) => callback(error.message));
 };
